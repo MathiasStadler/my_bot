8964fc24fe5d0485cdb16d95ef0f24185d696173
Mathias <stadler-mathias@web.de>
2024-03-18 15:17:36 +0100

    repeat funzt

--- examples/command_third.rs
+++ examples/command_third.rs
@@ -1,11 +1,5 @@
-// tokio repeat fn call
-// https://stackoverflow.com/questions/66863385/how-can-i-use-tokio-to-trigger-a-function-every-period-or-interval-in-seconds
-
-use std::time::Duration;
 use teloxide::{prelude::*, utils::command::BotCommands};
-// use tokio::{task, time}; // 1.3.0
-
-// use std::time::Duration;
+use std::time::Duration;
 use tokio::time::sleep;
 
 #[tokio::main]
@@ -15,23 +9,13 @@ async fn main() {
 
     let bot = Bot::from_env();
 
-    // REPL ("read-eval-print loop")
     Command::repl(bot, answer).await;
-
-    let val = std::cell::Cell::new(1);
-    tokio::select! {
-      _ = async {loop {
-        println!(".{}", val.get());
-        sleep(Duration::from_secs(5)).await;
-        println!("{}","hallo");
-      }} => {},
-    }
 }
 
 #[derive(BotCommands, Clone)]
 #[command(
     rename_rule = "lowercase",
-    description = "These commands are supported:"
+    description = "my_bot * command_third => These commands are supported:"
 )]
 enum Command {
     #[command(description = "display this text.")]
@@ -40,24 +24,13 @@ enum Command {
     Username(String),
     #[command(description = "handle a ticker symbol.")]
     Symbol(String),
-    #[command(description = "repeat the command fix time period.")]
-    Repeat,
     #[command(description = "handle a username and an age.", parse_with = "split")]
     UsernameAndAge { username: String, age: u8 },
-    #[command(description = "handle a username and an age.", parse_with = "split")]
-    RepeatEach { each: u8 },
+    #[command(description = "repeat ticker symbol.")]
+    Repeat,
 }
 
-#[allow(dead_code)]
-#[allow(unused_variables)]
-async fn send_repeat_msg(bot: Bot, msg: Message, cmd: Command) -> ResponseResult<()> {
-    bot.send_message(
-        msg.chat.id,
-        format!("TEST repeat msg => Your username is => username."),
-    )
-    .await?;
-    Ok(())
-}
+
 
 async fn answer(bot: Bot, msg: Message, cmd: Command) -> ResponseResult<()> {
     match cmd {
@@ -81,12 +54,19 @@ async fn answer(bot: Bot, msg: Message, cmd: Command) -> ResponseResult<()> {
             .await?
         }
         Command::Repeat => {
-            bot.send_message(msg.chat.id, format!("Repeat ticker"))
-                .await?
+            bot.send_message(msg.chat.id,format!("The repeat is active ."))
+                .await?;
+        
+        //start
+        loop{
+            sleep(Duration::from_secs(10)).await;
+            bot.send_message(msg.chat.id,format!("The repeat is active ."))
+                .await?;
+
         }
-        Command::RepeatEach { each } => {
-            bot.send_message(msg.chat.id, format!("Repeat ticker @{each} "))
-                .await?
+        
+        //end
+
         }
     };
 
